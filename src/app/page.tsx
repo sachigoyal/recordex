476837460796f848a447dfcda7f62c437b5311ab
Sachi Goyal <sachigoyal27@gmail.com>
2025-06-29 19:48:27 +0530

    theme toggle and github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import { Icons } from "@/components/icons";
+import ModeToggle from "@/components/ModeToggle";
 import ScreenRecorderApp from "@/components/ScreenRecoder";
 
 export default function Home() {
   return (
     <div>
-     <ScreenRecorderApp/>
+      <div className="absolute top-4 right-4 flex items-center gap-2">
+        <a
+          href="https://github.com/sachigoyal/recordex"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="p-3 hover:bg-gray-100 dark:hover:bg-gray-900 rounded-full transition-colors"
+        >
+          <Icons.Github className="w-5 h-5" />
+        </a>
+        <ModeToggle className="border-none bg-transparent hover:bg-gray-100 cursor-pointer rounded-full" />
+      </div>
+      <ScreenRecorderApp />
     </div>
   );
 }
